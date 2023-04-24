f7041ddef739050da458a7fdc9efa910064da102
ARRAJ Mohamed <118699556+mimidoox@users.noreply.github.com>
2023-04-24 17:53:05 +0100

    commit

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className="flex flex-col py-5 px-[10px] lg:px-[180px] 2xl:px-[300px] w-full">
         <div className="shadow py-5 px-7 space-y-3">
-            <h1 className="text-Cblue text-center md:text-left font-poppins font-black text-xl md:text-2xl">Je suis ARRAJ Mohamed</h1>
+            
             <p className='text-justify text-sm md:text-md'>
             Je m'appelle Mohamed, je suis étudiant en deuxième année de cycle d'ingénieur en informatique et réseaux à l'EMSI. Depuis mon plus jeune âge, j'ai toujours été fascinée par la technologie et l'informatique en particulier. J'ai donc décidé de poursuivre mes études dans ce domaine afin de me perfectionner et devenir un ingénieure qualifié.
 
